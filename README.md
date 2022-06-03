d3673328a93812b2054f1d3b528d0409c2b55acc
Shiqiang <cocowool@qq.com>
2022-06-03 19:42:55 +0800

    modify blog lists

--- README.md
+++ README.md
@@ -9,7 +9,27 @@ If you have suggestion, please edit the yaml and send me pull request.
 #  中文 / Chinese
 
 ## 个人博客
-
+* [泫言](https://blog.cugxuan.cn)
+* [旺阳](https://lqwang.net)
+* [lucifer的网络博客](https://lucifer.ren/blog/)
+* [zhangguanzhang's Blog](https://zhangguanzhang.github.io)
+* [宇宙湾 - 厚积薄发](https://yuzhouwan.com)
+* [雅绿](https://yalv.me)
+* [Dorad's Life](https://blog.cuger.cn)
+* [Qiu's Blog - 可以的话 再给我一点时间](https://blog.lyneee.com)
+* [五斤](https://www.chairyfish.com)
+* [Hope](https://leohope.com)
+* [林德熙](http://blog.lindexi.com)
+* [宇宙湾 - 厚积薄发](https://yuzhouwan.com)
+* [一之笔的博客 | Yizibi Blog](https://yizibi.github.io)
+* [LinuxTOY](https://linuxtoy.org)
+* [奇客Solidot: 奇客的知识，重要的东西](https://www.solidot.org)
+* [36氪_让一部分人先看到未来](https://www.36kr.com)
+* [JerryQu 的小站](http://www.imququ.com)
+* [风雪之隅 - Laruence的博客](https://www.laruence.com)
+* [酷壳 – CoolShell.cn](https://coolshell.cn)
+* [https://www.aqee.net](https://www.aqee.net)
+* [云风的 BLOG](https://blog.codingnow.com)
 
 # English
 
--- blog_spider.py
+++ blog_spider.py
@@ -23,32 +23,41 @@ def get_html(url, method = "requests"):
     s = requests.session()
     s.keep_alive = False
     response = s.get(url, headers = my_headers, cookies = my_cookie)
-    print(response.apparent_encoding)
-    print(response.encoding)
-    # Reference : https://blog.csdn.net/weixin_45975639/article/details/123737275
-    response.encoding = response.encoding
+
+    # 判断网页编码
+    charset_data = chardet.detect( response.content )
+    if charset_data['encoding'].lower() != response.encoding.lower():
+        if charset_data['confidence'] > 0.9:
+            # Reference : https://blog.csdn.net/weixin_45975639/article/details/123737275
+            response.encoding = charset_data['encoding']
+        else:
+            charset = re.search(r'charset=["|\'](.*?)["|\']', response.text ).group(1).strip()
+            # print(charset)
+            if charset:
+                response.encoding = charset
+
 
     return response.text
 
 file_name = 'tech-blog-lists-cn.txt'
 fh = open(file_name, 'r', encoding='utf-8')
 
+markdown_text = ''
+
 line = fh.readline().strip()
 while line:
-    print(line)
+    # print(line)
     line = fh.readline().strip()
     if line:
         html = get_html(line)
 
-        # charset_data = chardet.detect( html )
-
-        # Find charset
-        # charset = re.search(r'charset=(.*)["|\s]', html ).group(1).strip()
-        # print(charset)
-
         html_content  = BeautifulSoup(html, 'html.parser')
         t_title = html_content.find_all('title')[0].get_text().strip()
+        if t_title == '':
+            t_title = line
 
-        print(t_title)
+        markdown_text += "* [" + t_title + "](" + line + ")\n"
 
 fh.close()
+
+print(markdown_text)
