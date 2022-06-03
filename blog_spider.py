64475220eea46e93bea4f1f95bd76427206b4c3d
Shiqiang <cocowool@qq.com>
2022-06-03 18:56:40 +0800

    modify blog lists

--- blog_spider.py
+++ blog_spider.py
@@ -6,9 +6,10 @@ import time
 import cgi
 import json
 import pypinyin
+import chardet
 
 # 读取中文博客列表清单，生成 Markdown 格式的列表
-def get_html(self, url, method = "requests"):
+def get_html(url, method = "requests"):
     # config = self.read_config()
     my_cookie = ''
 
@@ -22,7 +23,10 @@ def get_html(self, url, method = "requests"):
     s = requests.session()
     s.keep_alive = False
     response = s.get(url, headers = my_headers, cookies = my_cookie)
-    response.encoding = 'utf-8'
+    print(response.apparent_encoding)
+    print(response.encoding)
+    # Reference : https://blog.csdn.net/weixin_45975639/article/details/123737275
+    response.encoding = response.encoding
 
     return response.text
 
@@ -33,6 +37,18 @@ line = fh.readline().strip()
 while line:
     print(line)
     line = fh.readline().strip()
-    # html = get_html(line)
+    if line:
+        html = get_html(line)
+
+        # charset_data = chardet.detect( html )
+
+        # Find charset
+        # charset = re.search(r'charset=(.*)["|\s]', html ).group(1).strip()
+        # print(charset)
+
+        html_content  = BeautifulSoup(html, 'html.parser')
+        t_title = html_content.find_all('title')[0].get_text().strip()
+
+        print(t_title)
 
 fh.close()
